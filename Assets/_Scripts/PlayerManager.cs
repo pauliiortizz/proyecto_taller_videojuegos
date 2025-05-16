b8e3e2ddea84aba1c405c7448da59d902f77a172
sofioliveto <2215225@ucc.edu.ar>
2025-05-16 16:31:53 -0300

    Se creo la escena final y funcionalidades del player

--- Assets/_Scripts/PlayerManager.cs
+++ Assets/_Scripts/PlayerManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerManager : MonoBehaviour
 {
@@ -6,6 +7,8 @@ public class PlayerManager : MonoBehaviour
     public GameObject amuletIcon; // Imágen de amuleto en la UI
     private int keyNumber = 0; // Cont de llaves
     public bool HaveAmulet = false; // Bandera para verificar si el jugador tiene el amuleto
+    public bool HaveKeys = false; // Bandera para verificar si el jugador tiene llaves
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -25,11 +28,25 @@ public class PlayerManager : MonoBehaviour
             Debug.Log("Llave obtenida"); // Mensaje de depuración
             HaveAmulet=false; // Cambia la bandera a falso
         }
+
+        if (collision.gameObject.CompareTag("Wizard") && HaveKeys)
+        {
+            for (; keyNumber >= 0; keyNumber--)
+            {
+                keyIcons[keyNumber].SetActive(false); // Desactiva todas las llaves
+            }
+            Debug.Log("Llaves borradas"); // Mensaje de depuración
+            SceneManager.LoadScene(2); //Cambia la escena al final
+        }
     }
     public void GiveKey()
     {
         keyIcons[keyNumber].SetActive(true); // Activa solo la primera llave
         keyNumber++; // Incrementa el número de llaves
+        if (keyNumber >= keyIcons.Length)
+        {
+            HaveKeys = true; // Cambia la bandera a verdadero
+        }
     }
 
 
