95f3209948f8cacc4eb8f555f1d28f1d5df3af40
ArnonNahmias <2222270@ucc.edu.ar>
2025-05-16 17:16:33 -0300

    MENU READY

--- Assets/_Scripts/SceneManager.cs
+++ Assets/_Scripts/SceneManager.cs
@@ -1,15 +1,40 @@
 using UnityEngine;
+using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    [Tooltip("El componente Animation que tiene tu clip")]
+    public Animation targetAnimation;
+
+    public Animator textAnimator;
+    public Animator doorAnimator;
+    public Animator bgAnimator;
+
+    [Tooltip("El nombre exacto de tu clip")]
+    public string clipName;
 
     void Update()
     {
        if(Input.GetMouseButtonDown(0))
        {
-            SceneManager.LoadScene(1);
+           textAnimator.SetTrigger("Play");
+           doorAnimator.SetTrigger("Play");
+           bgAnimator.SetTrigger("Play");
+
+           
+           //ejecutar corrutina
+           StartCoroutine((WaitToAnimate()));   
+            
        } 
+    }
+
+    //corrutina
+    IEnumerator WaitToAnimate()
+    {
+        yield return new WaitForSeconds(1); //esperar 1 seg
+        
+        SceneManager.LoadScene(1); //ejecuta la accion
     }
 }
 
