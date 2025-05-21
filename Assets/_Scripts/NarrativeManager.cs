c4148c4385ad5c9a8a06f5feb796c61aa21ced39
marcosbugliotti <2207479@ucc.edu.ar>
2025-05-21 00:24:48 -0300

    cambios de formas dialogos

--- Assets/_Scripts/NarrativeManager.cs
+++ Assets/_Scripts/NarrativeManager.cs
@@ -1,16 +1,60 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class NarrativeManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static NarrativeManager instance{ get; private set; }
+
+    Dictionary<string, bool> pearls = new Dictionary<string, bool>{
+        {"Cofre", false},
+        {"Estatua", false},
+        {"Runas", false},
+        {"Mago", false},
+    };
+
+   
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+   
+    public void SetPearl(string pearlName, bool value)
     {
-        
+        if (pearls.ContainsKey(pearlName))
+        {
+            pearls[pearlName] = value;
+        }
+        else
+        {
+            Debug.LogWarning("Pearl not found: " + pearlName);
+        }
     }
+
+    public bool GetPearl(string pearlName)
+    {
+        if (pearls.ContainsKey(pearlName))
+        {
+            return pearls[pearlName];
+        }
+        else
+        {
+            Debug.LogWarning("Pearl not found: " + pearlName);
+            return false;
+        }
+    }
+    //Singleton
+
+    //GetKey
+
+    //SetKey
+    
 }
--- Assets/_Scripts/dialogos.cs
+++ Assets/_Scripts/dialogos.cs
@@ -19,57 +19,6 @@ public class dialogos : MonoBehaviour
         MostrarDialogoActual();
     }
 
-    void SiguienteDialogo()
-    {
-        indiceDialogoActual++;
-        if (indiceDialogoActual < dialogosArray.Length)
-        {
-            MostrarDialogoActual();
-        }
-        else
-        {
-            Debug.Log("Fin de los diálogos");
-            canvas.SetActive(false);
-        }
-    }
-
-    void MostrarDialogoActual()
-    {
-        textoDialogo.text = dialogosArray[indiceDialogoActual].texto;
-        textoNombre.text = dialogosArray[indiceDialogoActual].nombre;
-    }
-
-   // En dialogos.cs, modifica el OnTriggerEnter2D
-void OnTriggerEnter2D(Collider2D other)
-{
-    Debug.Log("El jugador ha entrado en el trigger");
-
-    if (other.CompareTag("Player"))
-    {
-        // Mostrar diálogo
-        indiceDialogoActual = 0;
-        canvas.SetActive(true);
-        MostrarDialogoActual();
-        
-        // Notificar al PlayerManager sobre la interacción con la estatua
-        PlayerManager player = other.GetComponent<PlayerManager>();
-        if (player != null && player.HaveAmulet)
-        {
-            player.GiveKey();
-            player.HaveAmulet = false;
-            player.amuletIcon.SetActive(false);
-        }
-    }
-}
-
-    void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            canvas.SetActive(false);
-        }
-    }
-
     void Start()
     {
         // Verificar que el collider está configurado como trigger
@@ -83,6 +32,117 @@ void OnTriggerEnter2D(Collider2D other)
             Debug.LogError("No se encontró un Collider2D en este objeto");
         }
     }
+
+    void SiguienteDialogo()
+    {
+        /*indiceDialogoActual++;
+        //parametro clave
+        if (indiceDialogoActual < dialogosArray.Length)
+        {
+            MostrarDialogoActual();
+        }
+        else
+        {
+            Debug.Log("Fin de los diálogos");*/
+            canvas.SetActive(false);
+       // }
+    }
+
+    void MostrarDialogoActual()
+    {
+      textoDialogo.text = dialogosArray[indiceDialogoActual].texto;
+        textoNombre.text = dialogosArray[indiceDialogoActual].nombre;
+    }
+
+    // En dialogos.cs, modifica el OnTriggerEnter2D
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("El jugador ha entrado en el trigger");
+
+        if (other.CompareTag("Dialog"))
+        {
+            if(other.gameObject.name == "Cofre") //ojoal guardar escena y q no tenga el mismo nombr eel cofre
+            {
+                string keyToEvaluate = "Cofre";
+                bool keyStatus = NarrativeManager.instance.GetPearl(keyToEvaluate);
+
+                if(keyStatus)
+                {
+                    indiceDialogoActual = 1;
+
+                    canvas.SetActive(true);
+                    MostrarDialogoActual();
+                }
+                else
+                {
+                    indiceDialogoActual = 0;
+
+                    canvas.SetActive(true);
+                    MostrarDialogoActual();
+                }
+            }
+
+            if(other.gameObject.name == "Estatua") //ojoal guardar escena y q no tenga el mismo nombr eel cofre
+            {
+              string keyToEvaluate = "Estatua";
+                bool keyStatus = NarrativeManager.instance.GetPearl(keyToEvaluate);
+
+                if(keyStatus)
+                {
+                    indiceDialogoActual = 3;
+
+                    canvas.SetActive(true);
+                    MostrarDialogoActual();
+                }
+                else
+                {
+                    indiceDialogoActual = 2;
+
+                    canvas.SetActive(true);
+                    MostrarDialogoActual();
+                }
+            }
+
+             if(other.gameObject.name == "Runas") //ojoal guardar escena y q no tenga el mismo nombr eel cofre
+            {
+              string keyToEvaluate = "Runas";
+                bool keyStatus = NarrativeManager.instance.GetPearl(keyToEvaluate);
+
+                if(keyStatus)
+                {
+                    indiceDialogoActual = 5;
+
+                    canvas.SetActive(true);
+                    MostrarDialogoActual();
+                }
+                else
+                {
+                    indiceDialogoActual = 4;
+
+                    canvas.SetActive(true);
+                    MostrarDialogoActual();
+                }
+            }
+
+            
+            // Notificar al PlayerManager sobre la interacción con la estatua
+            PlayerManager player = other.GetComponent<PlayerManager>();
+            if (player != null && player.HaveAmulet)
+            {
+                player.GiveKey();
+                player.HaveAmulet = false;
+                player.amuletIcon.SetActive(false);
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canvas.SetActive(false);
+        }
+    }
 }
 [System.Serializable]
 public class BurbujaText
@@ -90,5 +150,6 @@ public class BurbujaText
     [TextArea(0, 100)]
     public string texto;
     public string nombre;
-
+    public string keyToEvaluate;
+    public bool keyStatus;
 }
